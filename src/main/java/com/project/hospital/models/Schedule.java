2836c5984974a6e25b68b4151c805e33367061e0
akbarhassan <akbar.alsaleh@gmail.com>
2026-01-04 23:23:01 +0300

    add schedule

--- src/main/java/com/project/hospital/models/Schedule.java
+++ src/main/java/com/project/hospital/models/Schedule.java
@@ -1,4 +1,48 @@
 package com.project.hospital.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "schedules")
 public class Schedule {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column
+    private String purpose;
+
+    @CreationTimestamp
+    private LocalDateTime bookedAt;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+
 }
