b9d61e14e55fa562a408902fbdd4a0cbbced6210
akbarhassan <akbar.alsaleh@gmail.com>
2026-01-04 22:48:10 +0300

    create user model

--- src/main/java/com/project/hospital/models/User.java
+++ src/main/java/com/project/hospital/models/User.java
@@ -1,4 +1,38 @@
 package com.project.hospital.models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import jakarta.persistence.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String userName;
+
+
+    @Column(unique = true)
+    private String emailAddress;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+
+
+
+
 }
