84bc8539e59451afb0019165bcca5f62e7101366
akbarhassan <akbar.alsaleh@gmail.com>
2026-01-04 22:48:24 +0300

    fix role model

--- src/main/java/com/project/hospital/models/Role.java
+++ src/main/java/com/project/hospital/models/Role.java
@@ -1,4 +1,40 @@
 package com.project.hospital.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+
 public class Role {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    // generate permissions for rbac
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions = new HashSet<>();
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<User> users = new HashSet<>();
+
 }
