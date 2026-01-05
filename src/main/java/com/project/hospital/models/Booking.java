e4bd79763238f54894ade572be48dc7ec2900b8a
Ali-Alaalem <meemo2002a@gmail.com>
2026-01-05 20:00:43 +0300

    add new booking model, add relation with appointment model and user

--- src/main/java/com/project/hospital/models/Booking.java
+++ src/main/java/com/project/hospital/models/Booking.java
@@ -1,4 +1,34 @@
 package com.project.hospital.models;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bookings")
+
 public class Booking {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private LocalDateTime bookedAt;
+
+
+    @OneToOne(mappedBy = "booking")
+    private Appointment appointment;
+
+
 }
