e640f084eb1da16b5da930027007871ff5b3a2c4
Ali-Alaalem <meemo2002a@gmail.com>
2026-01-07 19:23:42 +0300

    adding the Token model for email cerification

---
+++ src/main/java/com/project/hospital/models/Token.java
@@ -0,0 +1,24 @@
+package com.project.hospital.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token")
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+
+    @OneToOne
+    private User user;
+
+}
